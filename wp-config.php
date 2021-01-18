c87e849e009d2fed4856d5dc43f3354c84953f4c
LAPTOP-2HM0ARKO\sotow <sotoworld@hotmail.com>
2021-01-18 19:20:46 +0100

    equiposadfasgd

--- wp-config.php
+++ wp-config.php
@@ -26,10 +26,11 @@ define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Zx171260225!' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '92.38.171.129' );
+
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
